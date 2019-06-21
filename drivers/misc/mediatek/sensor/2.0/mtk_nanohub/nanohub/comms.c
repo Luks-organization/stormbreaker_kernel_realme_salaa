d86cb6f2c2672c2ac31e86b3dbbc5d6a01db607f
Jason A. Donenfeld <Jason@zx2c4.com>
2019-06-21 22:32:48 +0200

    BACKPORT: timekeeping: Use proper clock specifier names in functions
    This makes boot uniformly boottime and tai uniformly clocktai, to
    address the remaining oversights.
    
    Change-Id: I3463b9045bddeba00d6f9fcf78d63008459c1b9a
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lkml.kernel.org/r/20190621203249.3909-2-Jason@zx2c4.com

--- drivers/misc/mediatek/sensor/2.0/mtk_nanohub/nanohub/comms.c
+++ drivers/misc/mediatek/sensor/2.0/mtk_nanohub/nanohub/comms.c
@@ -321,8 +321,7 @@ int nanohub_comms_rx_retrans_boottime(struct nanohub_data *data,
 
 	do {
 		data->comms.open(data);
-		get_monotonic_boottime(&ts);
-		boottime = timespec_to_ns(&ts);
+		boottime = ktime_get_boottime_ns();
 		packet_size =
 		    packet_create(&pad->packet, seq, cmd, sizeof(boottime),
 				  (u8 *)&boottime, false);
