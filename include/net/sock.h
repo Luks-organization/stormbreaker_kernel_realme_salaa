5e2a299a5214ab8066d53dd6421e793cd8bb3815
Ferenc Fejes <fejes@inf.elte.hu>
2020-05-30 23:09:00 +0200

    BACKPORT: net: Make locking in sock_bindtoindex optional
    The sock_bindtoindex intended for kernel wide usage however
    it will lock the socket regardless of the context. This modification
    relax this behavior optionally: locking the socket will be optional
    by calling the sock_bindtoindex with lock_sk = true.
    
    The modification applied to all users of the sock_bindtoindex.
    
    Change-Id: Ief99636cf1a326099a80e0d9e75651827db22e27
    Signed-off-by: Ferenc Fejes <fejes@inf.elte.hu>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/bee6355da40d9e991b2f2d12b67d55ebb5f5b207.1590871065.git.fejes@inf.elte.hu
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2610,6 +2610,6 @@ int sock_bind_add(struct sock *sk, struct sockaddr *addr, int addr_len);
 /* On 32bit arches, an skb frag is limited to 2^15 */
 #define SKB_FRAG_PAGE_ORDER	get_order(32768)
 
-int sock_bindtoindex(struct sock *sk, int ifindex);
+int sock_bindtoindex(struct sock *sk, int ifindex, bool lock_sk);
 
 #endif	/* _SOCK_H */
